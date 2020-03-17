687657df66a2ab8fb1b9e081c20658623c7317e0
Dylan DeRouchie <dylan.f.derouchie@gmail.com>
2020-03-17 16:19:25 -0500

    finished up base logic for each of the methods. just making sure everything works. also added bootstrap theme, working on adding a photo to the home page of favorite hero.

--- SuperheroCreator/Controllers/HomeController.cs
+++ SuperheroCreator/Controllers/HomeController.cs
@@ -22,7 +22,22 @@ namespace SuperheroCreator.Controllers
         {
             return View();
         }
-
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
--- SuperheroCreator/Controllers/SuperherosController.cs
+++ SuperheroCreator/Controllers/SuperherosController.cs
@@ -4,26 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SuperheroCreator.Data;
 using SuperheroCreator.Models;
 
 namespace SuperheroCreator.Controllers
 {
     public class SuperherosController : Controller
     {
+        public ApplicationDbContext _context;
+        public SuperherosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: Superheros
         public ActionResult Index()
         {
-            for (int i = 0; i < ; i++)
-            {
-
-            }
-            return View();
+            var superheroes = _context.Superheroes.ToList();
+            return View(superheroes);
         }
 
         // GET: Superheros/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            Superhero superhero = _context.Superheroes.Where(s => s.Id == id).SingleOrDefault();
+            return View(superhero);
         }
 
         // GET: Superheros/Create
@@ -41,7 +45,8 @@ namespace SuperheroCreator.Controllers
             try
             {
                 // TODO: Add insert logic here
-
+                _context.Superheroes.Add(superhero);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -53,18 +58,31 @@ namespace SuperheroCreator.Controllers
         // GET: Superheros/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Superhero superhero = _context.Superheroes.Where(s => s.Id == id).SingleOrDefault();
+            return View(superhero);
         }
 
         // POST: Superheros/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Superhero superhero)
         {
             try
             {
                 // TODO: Add update logic here
+                //update Superheroes table with 
+                var superheroInDB = _context.Superheroes.Where(v => v.Id == id).SingleOrDefault();
 
+                //option 1 -- maybe? need to test
+                superheroInDB = superhero;
+
+                //option 2 -- setting individual props (getting better)
+                //superheroInDB.AlterEgo = superhero.AlterEgo;
+
+                //option 3 -- best option?
+                //_context.Superheroes.Update(superhero);
+
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -76,18 +94,20 @@ namespace SuperheroCreator.Controllers
         // GET: Superheros/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Superhero superhero = _context.Superheroes.Where(s => s.Id == id).SingleOrDefault();
+            return View(superhero);
         }
 
         // POST: Superheros/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Superhero superhero)
         {
             try
             {
                 // TODO: Add delete logic here
-
+                _context.Superheroes.Remove(superhero);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
